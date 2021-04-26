6cdef2599367f7f451f6285af97e9e654572d7ae
Cat Ormerod <catormerod@gmail.com>
2021-04-26 13:46:49 +1000

    continued work into Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // projects object //
 var projectsContent = [
     { 
+        projectID: "1",
         projectTitle: "Workday Scheduler",
         projectDescription: "App to display the workday hours where user can save their appointments",
         projectTools: "HTML, CSS, JavaScript, Bootstrap, GoogleFonts",
@@ -8,6 +9,7 @@ var projectsContent = [
         projectURL: "https://catormerod.github.io/work-day-scheduler/",
     },
     { 
+        projectID: "2",
         projectTitle: "Weather App",
         projectDescription: "Weather app that provides the weather for any location and features a geolocation on opening the page to show the weather in the users current location.",
         projectTools: "HTML, CSS, JavaScript, jQuery, API, Bootstrap, Font Awesome",
@@ -15,20 +17,37 @@ var projectsContent = [
         projectURL: "https://catormerod.github.io/weather-dashboard/",
     },
     { 
+        projectID: "3",
         projectTitle: "Tipsy Tricks - Group Project",
         projectDescription: "A home bar assistant to take any entered ingredient and display cocktails that use that ingredient.  Further to this it takes the users current location and provides a list of Liquor Stores in a 5km radius in case they need more ingredients.",
         projectTools: "HTML, CSS, JavaScript, jQuery, API, Bootstrap, Font Awesome",
         projectRepo: "https://github.com/RichardtHopkins/Cocktail-suggester",
         projectURL: "https://richardthopkins.github.io/Cocktail-suggester/",
-    }],
+}],
 
+const myModal = document.getElementById('myModal')
 
-$('#myModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    let i = button.data('index') // Extract info from data-* attributes
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('#modal-project-title').text(projectsContent[i].projectTitle)
-    $('#modal-project-description').html(projectsContent[i].projectDescription)
+myModal.addEventListener('show.bs.modal', (event) => {
+    const button = event.relatedTarget
+    const projectID = button.getAttribute('data-id')
+    const modalTitle = myModal.querySelector("#modal-project-title")
+    const modalDescription = myModal.querySelector("#modal-project-description")
 
-  })
+    modalTitle.textContent = `${projectID}`
+    modalDescription.html = 
+})
+//     var modal = $(this)
+//     let i = button.data('index')
+//     modal.find('#modal-project-title').text(projectsContent[i].projectTitle)
+//     $('#modal-project-description').html(projectsContent[i].projectDescription)  
+// });
+
+// $('#myModal').on('show.bs.modal', function (event) {
+//     var button = $(event.relatedTarget) // Button that triggered the modal
+//     let i = button.data('index') // Extract info from data-* attributes
+//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//     var modal = $(this)
+//     modal.find('#modal-project-title').text(projectsContent[i].projectTitle)
+//     $('#modal-project-description').html(projectsContent[i].projectDescription)
+
+//   })
