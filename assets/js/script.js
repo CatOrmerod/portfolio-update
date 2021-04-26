fa6b4413afa6bb953e591e36dacf864c893a5fcd
Cat Ormerod <catormerod@gmail.com>
2021-04-26 17:58:33 +1000

    intial work on fullscreen done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,30 @@
 // projects object //
-var projectsContent = [
-    { 
-        projectID: "1",
+var projectsContent = {
+    1: {
         projectTitle: "Workday Scheduler",
         projectDescription: "App to display the workday hours where user can save their appointments",
         projectTools: "HTML, CSS, JavaScript, Bootstrap, GoogleFonts",
         projectRepo: "https://github.com/CatOrmerod/work-day-scheduler",
         projectURL: "https://catormerod.github.io/work-day-scheduler/",
     },
-    { 
-        projectID: "2",
+    2: { 
         projectTitle: "Weather App",
         projectDescription: "Weather app that provides the weather for any location and features a geolocation on opening the page to show the weather in the users current location.",
         projectTools: "HTML, CSS, JavaScript, jQuery, API, Bootstrap, Font Awesome",
         projectRepo: "https://github.com/CatOrmerod/weather-dashboard",
         projectURL: "https://catormerod.github.io/weather-dashboard/",
     },
-    { 
-        projectID: "3",
+    3: { 
         projectTitle: "Tipsy Tricks - Group Project",
         projectDescription: "A home bar assistant to take any entered ingredient and display cocktails that use that ingredient.  Further to this it takes the users current location and provides a list of Liquor Stores in a 5km radius in case they need more ingredients.",
         projectTools: "HTML, CSS, JavaScript, jQuery, API, Bootstrap, Font Awesome",
         projectRepo: "https://github.com/RichardtHopkins/Cocktail-suggester",
         projectURL: "https://richardthopkins.github.io/Cocktail-suggester/",
-}];
-//VERSION 3
-// const myModal = document.getElementById('myModal')
-
-// myModal.addEventListener('show.bs.modal', (event) => {
-//     const button = event.relatedTarget
-//     const projectID = button.getAttribute('data-id')
-//     const modalTitle = myModal.querySelector("#modal-project-title")
-//     const modalDescription = myModal.querySelector("#modal-project-description")
+    }
+}
 
 
-//     modalTitle.textContent = projectsContent[projectID].projectTitle
-//     modalDescription.html = projectsContent[projectID].projectDescription
-    //modalDescription.html = 
-// })
-// VERSION 2
-//     var modal = $(this)
-//     let i = button.data('index')
-//     modal.find('#modal-project-title').text(projectsContent[i].projectTitle)
-//     $('#modal-project-description').html(projectsContent[i].projectDescription)  
-// });
-
-// VERSION 1
+// Update the content of the Modal with information from the object array
 $('#myModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     let projectID = button.data('id') // Extract info from data-* attributes
@@ -54,5 +33,14 @@ $('#myModal').on('show.bs.modal', function (event) {
     modal.find('#modal-project-title').text(projectsContent[projectID].projectTitle)
     $('#modal-project-description').html(projectsContent[projectID].projectDescription)
     $('#modal-project-tools').html(projectsContent[projectID].projectTools)
+    //populate buttons with the urls so when clicked will take to the relevant websites
+    $("#modal-repo").on("click", function(){
+        window.open(projectsContent[projectID].projectRepo)
+    });
+    
+    $("#modal-live-page").on("click", function(){
+        window.open(projectsContent[projectID].projectURL)
+    });
 
 })
+
